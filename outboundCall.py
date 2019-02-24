10fd01c149d4e6ea71179ee90c84a3896f2809cb
ps24 <pshieh@csu.fullerton.edu>
2019-02-23 21:13:43 -0800

    add Twilio functions

--- outboundCall.py
+++ outboundCall.py
@@ -1,3 +1,7 @@
+from twilio.rest import Client
+from twilio.twiml.voice_response import Dial, VoiceResponse, Say
+
+
 class OutboundCall(object):
     def dial(self, company):
         pass
@@ -7,7 +11,17 @@ class Twilio(OutboundCall):
         super(Twilio, self).__init__()
 
     def dial(self, company):
-        print("Dialing with Twilio  " + company.phoneNum)
+        print("Dialing " + company.phoneNum)
+        # set the method to "GET" from default POST
+        # serves GET requests on files. Typically POST would be used for apps
+        client.calls.create(
+            to=company.phoneNum,
+            from_=TWILIO_PHONE_NUMBER,
+            url=TWIML_INSTRUCTIONS_URL
+        )
+
+    # def dial(self, company):
+    #     print("Dialing with Twilio  " + company.phoneNum)
 
 class GoogleVoice(OutboundCall):
     def __init__(self):
