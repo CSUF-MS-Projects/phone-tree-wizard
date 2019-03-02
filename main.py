14de491fb9c690a38e6b4ee4b81110d68cd56b01
ps24 <pshieh@csu.fullerton.edu>
2019-03-01 20:08:25 -0800

    add parseSpeech function calls

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import company
 import outboundCall
+import parseSpeech
+
+
 
 input_company = input("Enter company name: ")
 input_phone = input("Enter phone number to dial: ")
 
 # initial set up
-c1 = company.RegularCompany(phoneNum=input_phone, companyName=input_company, outbound_method=outboundCall.Twilio())
+c1 = company.RegularCompany(phoneNum=input_phone, companyName=input_company, outbound_method=outboundCall.Twilio(), parse_method=parseSpeech.Twilio())
 c1.display()
 
 # call various outbound methods
 print('\n')
 c1.dial(c1)
 print("outro")
+c1.parse(c1)
 
-#  switch outbound_method to GoogleVoice
+# switch outbound_method to GoogleVoice
 c1.outbound_method = outboundCall.GoogleVoice()
 c1.dial(c1)
