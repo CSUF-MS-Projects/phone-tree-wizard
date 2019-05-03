58b17246f4228a9bdbacde7cf2e16273af55d6ed
ps24 <pshieh@csu.fullerton.edu>
2019-05-03 10:54:17 -0700

    clean up comments

--- outgoing.py
+++ outgoing.py
@@ -1,4 +1,3 @@
-# source: https://www.youtube.com/watch?v=uCT-sAbwIDY
 import os
 from flask import Flask, request
 from twilio.rest import Client
@@ -9,14 +8,12 @@ TWILIO_API = os.environ.get('TWILIO_API')
 TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
 
 # initialize ngrok URL
-ngrok_url = "http://25503142.ngrok.io/"  # INSERT NGROK URL...include /
+ngrok_url = "[insert at runtime]"  # INSERT NGROK URL...include /
 
 app = Flask(__name__)
 
 client = Client(TWILIO_API, TWILIO_AUTH_TOKEN)
 
-# input phone number
-#num = input("Enter phone number to call: ")
 
 # Make outgoing voice call
 client.calls.create(
@@ -39,22 +36,13 @@ def voice():
     return str(twiml)
 
 
-########################################################
-# UPDATE 3/19 @ 4:30PM: able to dial another number during the call
-# potential issue:  this can dial a new phone number --> but will it do it for single digits?
-########################################################
 # Handle POST from <Gather> action reach out final result
 @app.route('/store', methods=['GET', 'POST'])
 def callback():
     twiml = VoiceResponse()
     speech_result = request.values.get('SpeechResult', None)
     print('Speech Result: ' + speech_result)
-    # if 'open sesame' in speech_result:
-    #     twiml.say('Welcome home!')
-    #     twiml.dial()
-    # else:
-    #     twiml.say("That's not the answer")
-    #     twiml.dial("+1")
+
     # print('Speech Result: ' + speech_result)
     twiml.hangup()  # hang up call after gathering speech
     return str(twiml)
