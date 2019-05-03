1e441b3d8de81b57ab91c925447cd56db62bdc60
ps24 <pshieh@csu.fullerton.edu>
2019-05-03 10:49:16 -0700

    return dict value

--- company.py
+++ company.py
@@ -1,5 +1,12 @@
+"""
+Purpose:  Initialize all parent-to-child classes (inheritance)
+"""
+
+
+# import libraries
 import outboundCall
 import parse
+import treeDiagram
 
 class Company(object):
     def __init__(self, phoneNum=None, companyName=None):
@@ -7,6 +14,7 @@ class Company(object):
         self.companyName = companyName
         self.outbound_method = outboundCall.OutboundCall()
         self.parse_method = parse.ParseText()
+        self.treeDiagram_method = treeDiagram.drawTreeDiagram()
 
     def display(self):
         pass
@@ -15,14 +23,22 @@ class Company(object):
         self.outbound_method.dial(company)
 
     def parse(self, company, text):
-        self.parse_method.parse(company, text)
+        return self.parse_method.parse(company, text)
 
+    def drawDiagram(self, company):
+        self.treeDiagram_method.drawDiagram(company)
 
 class RegularCompany(Company):
-    def __init__(self, phoneNum=None, companyName=None, outbound_method=None, parse_method=None):
+    def __init__(self,
+                 phoneNum=None,
+                 companyName=None,
+                 outbound_method=None,
+                 parse_method=None,
+                 treeDiagram_method=None):
         super(RegularCompany, self).__init__(phoneNum, companyName)
         self.outbound_method = outbound_method
         self.parse_method = parse_method
+        self.treeDiagram_method = treeDiagram_method
 
     def display(self):
-        print("Regular company " + self.companyName + "   ----  " + self.phoneNum)
+        print("Calling company: " + self.companyName + "   // Phone Number:  " + self.phoneNum)
